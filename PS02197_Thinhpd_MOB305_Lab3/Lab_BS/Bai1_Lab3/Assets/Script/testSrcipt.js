31a0686a0028d5ecf0a82f9937384002404da2eb
thinhpdps02197 <thinhpdps02197@fpt.edu.vn>
2015-08-01 13:45:14 +0700

    Test

---
+++ PS02197_Thinhpd_MOB305_Lab3/Lab_BS/Bai1_Lab3/Assets/Script/testSrcipt.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+var animator : Animator;
+function Start () {
+animator = GetComponent(Animator);
+}
+
+function Update () {
+	if(Input.GetKeyDown(KeyCode.Space)){
+	ManJump();
+	}
+}
+function ManJump(){
+	animator.SetBool("jumping",true);
+	yield WaitForSeconds(1);
+	animator.SetBool("jumping",false);
+	animator.Play("main");
+}
